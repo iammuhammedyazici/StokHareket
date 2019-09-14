819e6dabd6f73e64ff0add741d5e831333b8c118
iammuhammedyazici <yzcmhmmd58@gmail.com>
2019-09-14 04:43:12 +0300

    End Commit

---
+++ HareketSorgulama/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace HareketSorgulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        sqlbaglantisi bgl = new sqlbaglantisi();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Exec HAREKETLER", bgl.baglanti());
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            gridControl1.DataSource = dt;
+        }
+    }
+}
---
+++ HareketSorgulama/sqlbaglantisi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace HareketSorgulama
+{
+    class sqlbaglantisi
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection baglan = new SqlConnection(@"Data Source=DESKTOP-F20JGOS\SQLSERVER1;Initial Catalog=DboHareketSorgulama;Integrated Security=True");
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
